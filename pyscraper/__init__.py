25020b6dd6b11207f0b462ea01fdf67f6be5ce92
temeteke <temeteke@gmail.com>
2017-10-09 10:26:21 +0900

    Add WebFile

--- pyscraper/__init__.py
+++ pyscraper/__init__.py
@@ -6,6 +6,10 @@ import lxml.html
 import requests
 from selenium import webdriver
 from selenium.common.exceptions import ElementNotInteractableException, NoSuchElementException
+from retry import retry
+from pathlib import Path
+import re
+from tqdm import tqdm
 
 logger = logging.getLogger(__name__)
 
@@ -24,10 +28,10 @@ class WebPageNoSuchElementError(Exception):
 
 class WebPage(metaclass=ABCMeta):
     def __enter__(self):
-	return self
+        return self
 
     def __exit__(self, exc_type, exc_value, traceback):
-	pass
+        pass
 
     @property
     @abstractmethod
@@ -106,11 +110,11 @@ class WebPageSelenium(WebPage):
 class WebPagePhantomJS(WebPageSelenium):
     def __init__(self, url):
         super().__init__()
-        self.__url = url
+        self._url = url
 
     def __enter__(self):
         self.webdriver = webdriver.PhantomJS()
-        self.webdriver.get(self.__url)
+        self.webdriver.get(self._url)
         return self
 
     def __exit__(self, exc_type, exc_value, traceback):
@@ -119,7 +123,7 @@ class WebPagePhantomJS(WebPageSelenium):
 class WebPageFirefox(WebPageSelenium):
     def __init__(self, url):
         super().__init__()
-        self.__url = url
+        self._url = url
 
     def __enter__(self):
         self.display = pyvirtualdisplay.Display()
@@ -127,9 +131,110 @@ class WebPageFirefox(WebPageSelenium):
         firefox_capabilities = DesiredCapabilities.FIREFOX
         firefox_capabilities['marionette'] = True
         self.webdriver = webdriver.Firefox(capabilities=firefox_capabilities)
-        self.webdriver.get(self.__url)
+        self.webdriver.get(self._url)
         return self
 
     def __exit__(self, exc_type, exc_value, traceback):
         self.webdriver.quit()
         self.display.stop()
+
+
+class WebFileDownloadError(Exception):
+    pass
+
+class WebFile():
+    def __init__(self, url, session=None, headers={}, directory='.', filename=None):
+        self.url = url
+
+        if session:
+            self.session = session
+        else:
+            self.session = requests.Session()
+
+        self.session.headers.update(headers)
+
+        self.directory = Path(directory)
+        if not self.directory.exists():
+            self.directory.mkdir()
+
+        if not filename:
+            filename = url.split('/').pop()
+        self._filename = re.sub(r'[/:\s\*]', '_', filename)
+
+    @mproperty
+    @debug
+    def filename(self):
+        r = self.session.head(self.url, allow_redirects=True)
+        logger.debug("Response Headers: " + str(r.headers))
+        if 'Content-Disposition' in r.headers:
+            return re.findall('filename="(.+)"', r.headers['Content-Disposition'])[0]
+        else:
+            return self._filename
+
+    @mproperty
+    @debug
+    def filepath(self):
+        return Path(self.directory, self.filename)
+
+    @mproperty
+    @debug
+    def filepath_tmp(self):
+        return Path(str(self.filepath) + '.part')
+
+    def download(self):
+        logger.info("Downloading {}".format(self.url))
+
+        if self.filepath.exists():
+            logger.warning("{} is already downloaded.".format(self.filepath))
+            return
+
+        try_maxcount = 10
+        for i in range(1, try_maxcount+1):
+            logger.debug("Trying to download ({}/{})".format(i, try_maxcount))
+
+            if self.filepath_tmp.exists():
+                downloaded_size = self.filepath_tmp.stat().st_size
+                self.session.headers['Range'] = 'bytes={}-'.format(downloaded_size)
+            else:
+                downloaded_size = 0
+
+            try:
+                logger.debug("Request Headers: " + str(self.session.headers))
+                r = self.session.get(self.url, stream=True)
+                logger.debug("Response Headers: " + str(r.headers))
+                r.raise_for_status()
+            except requests.exceptions.HTTPError as e:
+                logger.warning(e)
+                if e.response.status_code == 416:
+                    if self.filepath_tmp.exists():
+                        logger.debug("Removing downloaded file.")
+                        self.filepath_tmp.unlink()
+                else:
+                    wait_random(maxsec=30)
+                continue
+            except (requests.exceptions.ConnectionError, requests.exceptions.ChunkedEncodingError) as e:
+                logger.warning(e)
+                wait_random(maxsec=30)
+                continue
+
+            total_size = int(r.headers['Content-Length'])
+            if 'Content-Range' in r.headers:
+                total_size = int(r.headers['Content-Range'].split('/')[-1])
+            if total_size == 0:
+                raise WebFileDownloadError
+
+            with tqdm(total=total_size, initial=downloaded_size, unit='B', unit_scale=True, dynamic_ncols=True, ascii=True) as pbar:
+                with self.filepath_tmp.open('ab') as f:
+                    for block in r.iter_content(1024):
+                        f.write(block)
+                        pbar.update(len(block))
+
+            if self.filepath_tmp.stat().st_size == total_size:
+                self.filepath_tmp.rename(self.filepath)
+                return
+            else:
+                logger.warning("The size of the downloaded file is wrong.")
+                wait_random(maxsec=30)
+                continue
+
+        raise WebFileDownloadError
