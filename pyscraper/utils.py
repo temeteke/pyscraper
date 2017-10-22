3b55d5f23a0708f7654da58002cf27c3a936cb10
temeteke <temeteke@gmail.com>
2017-10-22 17:56:53 +0900

    Change debug message

--- pyscraper/utils.py
+++ pyscraper/utils.py
@@ -8,6 +8,10 @@ HEADERS = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:56.0) Gec
 def debug(f):
     @wraps(f)
     def wrapper(*args, **kwds):
+        if args[1:]:
+            logger.debug("{}('{}')".format('.'.join([args[0].__class__.__name__, f.__name__]), ','.join(args[1:])))
+        else:
+            logger.debug("{}".format(f.__name__))
         result = f(*args, **kwds)
         if args[1:]:
             logger.debug("{}('{}') -> {}".format('.'.join([args[0].__class__.__name__, f.__name__]), ','.join(args[1:]), result))
