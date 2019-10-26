e662183016629513f83c8ba5ced1c0abcf2218c3
temeteke <temeteke@gmail.com>
2019-10-26 10:27:18 +0900

    Add Dockerfile

--- setup.py
+++ setup.py
@@ -1,18 +1,3 @@
-from setuptools import setup, find_packages
+from setuptools import setup
 
-setup(
-    name="pyscraper",
-    author="temeteke",
-    author_email="temeteke@gmail.com",
-    use_scm_version=True,
-    setup_requires=['setuptools_scm'],
-    packages=find_packages(),
-    install_requires = [
-        'requests',
-        'selenium',
-        'lxml',
-        'memoize',
-        'retry',
-        'tqdm',
-    ],
-)
+setup()
