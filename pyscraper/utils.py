0188822586e0f6662f04938ae85bdae0485211da
temeteke <temeteke@gmail.com>
2020-11-17 23:21:57 +0900

    Change User-Agent

--- pyscraper/utils.py
+++ pyscraper/utils.py
@@ -3,7 +3,7 @@ import logging
 
 logger = logging.getLogger(__name__)
 
-HEADERS = {'User-Agent': "Mozilla/5.0 (X11; Linux x86_64; rv:69.0) Gecko/20100101 Firefox/69.0"}
+HEADERS = {'User-Agent': "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/75.0.3764.0 Safari/537.36"}
 
 def debug(f):
     @wraps(f)
