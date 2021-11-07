898b63ca63767151ff99a43a18f19da93d0a689c
temeteke <temeteke@gmail.com>
2021-11-07 10:31:48 +0900

    Add HlsFile import

--- pyscraper/__init__.py
+++ pyscraper/__init__.py
@@ -1,3 +1,4 @@
 from .webpage import WebPageRequests, WebPagePhantomJS, WebPageFirefox, WebPageChrome, WebPageCurl, WebPageError, WebPageNoSuchElementError
 from .webfile import WebFile, WebFileCached, WebFileError, WebFileRequestError, WebFileSeekError
+from .hlsfile import HlsFileFfmpeg, HlsFileRequests, HlsFileError
 from .utils import HEADERS
