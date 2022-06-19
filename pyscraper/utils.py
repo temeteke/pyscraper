2ddbf98e0bbbec76a3dd0a83e4bc68f21840891d
temeteke <temeteke@gmail.com>
2022-06-19 01:43:56 +0000

    Change default header

--- pyscraper/utils.py
+++ pyscraper/utils.py
@@ -32,13 +32,13 @@ class RequestsMixin():
     def init_session(self, session, headers, cookies):
         if session:
             self.session = session
+            # sessionのheadersにない項目はデフォルトのHEADERSを設定する
+            for k, v in HEADERS.items():
+                self.session.headers.setdefault(k, v)
         else:
             self.session = requests.Session()
+            self.session.headers.update(HEADERS)
 
-        # sessionのheadersにない項目はデフォルトのHEADERSを設定する
-        for k, v in HEADERS.items():
-            self.session.headers.setdefault(k, v)
-         
         # headersで上書きする
         self.session.headers.update(headers)
 
